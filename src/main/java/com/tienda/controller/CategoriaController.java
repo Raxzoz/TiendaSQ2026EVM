03ce837e5cb1fdb887d85e9b7e295d87f77dcdd0
neo31 <neo31@NeoLAP>
2026-07-20 20:43:43 -0600

    Clase 4 corre todo , pero tuve que modificar algunas partes del POM para que mi tienda jalara el boostrap y el springboot

--- src/main/java/com/tienda/controller/CategoriaController.java
+++ src/main/java/com/tienda/controller/CategoriaController.java
@@ -35,4 +35,44 @@ public class CategoriaController {
         model.addAttribute("totalCategorias", categorias.size());
         return "/categoria/listado";
     }
-}   
+
+    @PostMapping("/guardar")
+    public String guardar(@Valid Categoria categoria, @RequestParam MultipartFile imagenFile, RedirectAttributes redirectAttributes) {
+
+        categoriaService.save(categoria, imagenFile);
+        redirectAttributes.addFlashAttribute("todoOk", messageSource.getMessage("mensaje.actualizado", null, Locale.getDefault()));
+
+        return "redirect:/categoria/listado";
+    }
+
+    @PostMapping("/eliminar")
+    public String eliminar(@RequestParam Integer idCategoria, RedirectAttributes redirectAttributes) {
+        String titulo = "todoOk";
+        String detalle = "mensaje.eliminado";
+        try {
+            categoriaService.delete(idCategoria);
+        } catch (IllegalArgumentException e) {
+            titulo = "error"; // Captura la excepción de argumento inválido para el mensaje de "no existe"
+            detalle = "categoria.error01";
+        } catch (IllegalStateException e) {
+            titulo = "error"; // Captura la excepción de estado ilegal para el mensaje de "datos asociados"
+            detalle = "categoria.error02";
+        } catch (Exception e) {
+            titulo = "error";  // Captura cualquier otra excepción inesperada
+            detalle = "categoria.error03";
+        }
+        redirectAttributes.addFlashAttribute(titulo, messageSource.getMessage(detalle, null, Locale.getDefault()));
+        return "redirect:/categoria/listado";
+    }
+
+    @GetMapping("/modificar/{idCategoria}")
+    public String modificar(@PathVariable("idCategoria") Integer idCategoria, Model model, RedirectAttributes redirectAttributes) {
+        Optional<Categoria> categoriaOpt = categoriaService.getCategoria(idCategoria);
+        if (categoriaOpt.isEmpty()) {
+            redirectAttributes.addFlashAttribute("error", messageSource.getMessage("categoria.error01", null, Locale.getDefault()));
+            return "redirect:/categoria/listado";
+        }
+        model.addAttribute("categoria", categoriaOpt.get());
+        return "/categoria/modifica";
+    }
+}
